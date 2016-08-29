4082116aa6d337bf702c600cafb0cea4e02037c1
sia.s.saj <sia.s.saj@gmail.com>
2016-08-30 00:13:18 +1000

    Fix require calls... rookie mistakes

--- lib/smsbroadcast/connection.rb
+++ lib/smsbroadcast/connection.rb
@@ -1,12 +1,14 @@
 require 'faraday'
 
-class Smsbroadcast::Connection
+module Smsbroadcast
+  class Connection
 
-  def initialize
-    @conn = Faraday.new(url: Smsbroadcast.configuration.url) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    def initialize
+      @conn = Faraday.new(url: Smsbroadcast.configuration.url) do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
     end
   end
 end
